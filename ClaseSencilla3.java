7a33cbea81833d01e0ca1001c4b22927b1935703
RubenIMFinal <51320726+RubenIMFinal@users.noreply.github.com>
2025-10-21 21:06:47 +0200

    Commit para subir ClaseSencilla3

---
+++ ClaseSencilla3.java
@@ -0,0 +1,10 @@
+public class ClaseSencilla3 {
+       public ClaseSencilla3() {
+   }
+
+   public static void main(String[] var0) {
+      System.out.println("¡Hola, Mundo! Clase Sencilla 3");
+   }
+    
+    
+}
