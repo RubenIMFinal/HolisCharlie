e4473ceace1980a2b7553aa1896fa8d5ce979801
RubenIMFinal <51320726+RubenIMFinal@users.noreply.github.com>
2025-10-21 21:32:26 +0200

    Commit para subir ClaseSencilla3 para comparar

--- ClaseSencilla3.java
+++ ClaseSencilla3.java
@@ -3,7 +3,7 @@ public class ClaseSencilla3 {
    }
 
    public static void main(String[] var0) {
-      System.out.println("¡Hola, Mundo! Clase Sencilla 3");
+      System.out.println("¡Hola, Mundo! Clase Sencilla 3. Y prueba para comparar en Github");
    }
     
     
