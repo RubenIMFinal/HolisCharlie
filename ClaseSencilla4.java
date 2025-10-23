4b32f6467977a39f4efe0644602b966de2bc0149
RubenIMFinal <51320726+RubenIMFinal@users.noreply.github.com>
2025-10-23 20:55:47 +0200

    añadir ClaseMergear

---
+++ ClaseSencilla4.java
@@ -0,0 +1,9 @@
+public class ClaseSencilla4 {
+           public ClaseSencilla4() {
+   }
+
+   public static void main(String[] var0) {
+      System.out.println("¡Hola, Mundo! Clase Sencilla 4. Y prueba para probar mergeo");
+   }
+    
+}
